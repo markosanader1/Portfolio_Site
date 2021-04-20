dd65fc6b34f1d34b2b82e861f9b93a692927c912
markosanader1 <msanader1993@gmail.com>
2021-04-20 11:20:37 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import smtplib
+import requests
+
+PASSWORD = 'nemaviselozinke1'
+MY_EMAIL = 'migservice30@gmail.com'
+
+
+app = Flask(__name__)
+connection = smtplib.SMTP('smtp.gmail.com',port=587)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/form-entry',methods=['GET','POST'])
+def recieve_data():
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["message"])
+    if request.method == 'POST':
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f'Subject:Nova poruka sa sajta!\n\nOd:{data["email"]}\nIme:{data["name"]}\nPoruka:{data["message"]}')
+        connection.close()
+        # return "<h1>Successfully sent your message</h1>"
+        # flash('Your message was successfully sent!')
+        return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
